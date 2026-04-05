ebe2836a4a8eb102f47eb0a475531349312a5df6
zukugk <woolridgejeff@proton.me>
2026-04-05 16:43:27 -0230

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/keyin/ticketnestbackend/TicketnestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.ticketnestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketnestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketnestBackendApplication.class, args);
+    }
+
+}

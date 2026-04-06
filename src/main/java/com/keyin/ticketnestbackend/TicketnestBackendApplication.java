007160c57d37eb0015e963f3c39fe6aba6d0aa9b
Magret <belloabiodun17@gmail.com>
2026-04-05 22:31:21 -0230

    Add application.properties to .gitignore file and create application-example.properties

--- src/main/java/com/keyin/ticketnestbackend/TicketnestBackendApplication.java
+++ src/main/java/com/keyin/ticketnestbackend/TicketnestBackendApplication.java
@@ -3,9 +3,25 @@ package com.keyin.ticketnestbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for the RESTful service,
+ * responsible for bootstrapping the Spring Boot application.
+ */
 @SpringBootApplication
 public class TicketnestBackendApplication {
 
+    /**
+     * Default constructor for the RestServiceApplication class.
+     * This constructor is required by Spring Boot to create an instance of the application.
+     */
+    public TicketnestBackendApplication() {
+    }
+
+    /**
+     * The entry point of the application, which starts the Spring Boot context.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         SpringApplication.run(TicketnestBackendApplication.class, args);
     }

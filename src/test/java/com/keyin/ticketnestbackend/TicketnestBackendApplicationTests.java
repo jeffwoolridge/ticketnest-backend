7a77ae9285822ac0d92fdab12075ae305ed11e00
Magret <belloabiodun17@gmail.com>
2026-04-05 22:41:23 -0230

    Remove test suite and add application.properties to gitignore file

--- src/test/java/com/keyin/ticketnestbackend/TicketnestBackendApplicationTests.java
+++ src/test/java/com/keyin/ticketnestbackend/TicketnestBackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.keyin.ticketnestbackend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TicketnestBackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.keyin.ticketnestbackend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TicketnestBackendApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
